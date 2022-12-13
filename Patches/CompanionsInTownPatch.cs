02caaac3c018bb47e1379b3ba15ab7f404e3e455
Axel Cawe <Cawe321Mods@gmail.com>
2022-12-13 20:19:44 +0800

    First Release

--- Patches/CompanionsInTownPatch.cs
+++ Patches/CompanionsInTownPatch.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using CompanionsInTown.Helpers;
+using HarmonyLib;
+using SandBox.CampaignBehaviors;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.Settlements;
+using TaleWorlds.CampaignSystem.Settlements.Locations;
 
 namespace CompanionsInTown.Patches
 {
-    internal class CompanionsInTownPatch
+    
+    [HarmonyPatch(typeof(CommonTownsfolkCampaignBehavior), "AddPeopleToTownCenter")]
+    public static class CompanionsInTownPatch
     {
+        [HarmonyPostfix]
+        static void Postfix(Settlement settlement, Dictionary<string, int> unusedUsablePointCount, bool isDayTime)
+        {
+            if (!settlement.IsTown)
+                return;
+
+            Location locationWithId = settlement.LocationComplex.GetLocationWithId("center");
+            foreach (var character in settlement.HeroesWithoutParty)
+            {
+                if (character.IsWanderer && character.CompanionOf == null && character.DeathMark != KillCharacterAction.KillCharacterActionDetail.Lost)
+                {
+                    locationWithId.AddCharacter(CompanionsInTownHelper.CreateWandererLocationCharacter(character));
+
+                }
+            }
+        }
     }
 }
