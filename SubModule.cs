3e28b4f57b608ffc15bd5ccbf75b2ebd6931c4e6
Axel Cawe <Cawe321Mods@gmail.com>
2022-12-13 12:32:45 +0800

    Initial commit

---
+++ SubModule.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.MountAndBlade;
+
+namespace CompanionsInTown
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnBeforeInitialModuleScreenSetAsRoot()
+        {
+            new Harmony("CompanionsInTown").PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
